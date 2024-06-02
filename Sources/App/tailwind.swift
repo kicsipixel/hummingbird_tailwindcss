4a2ac341cab114d1b3a40ad0fa09ccf5c084668b
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-06-02 03:04:37 +0200

    ApplicationProtocol extension was added.

--- Sources/App/tailwind.swift
+++ Sources/App/tailwind.swift
@@ -1,16 +1,44 @@
+import Foundation
 import Hummingbird
 import SwiftyTailwind
 import TSCBasic
 
 func tailwind(_ app: some ApplicationProtocol) async throws {
-    let resourceDirectory = try AbsolutePath(validating: "/Users/sztoth/Development/Hummingbird/tailwind/Resources")
-    let publicDirectory = try AbsolutePath(validating: "/Users/sztoth/Development/Hummingbird/tailwind/Public/")
+    let resourceDirectory = try AbsolutePath(validating: app.directory.resourcesDirectory)
+    let publicDirectory = try AbsolutePath(validating: app.directory.publicDirectory)
     
     let tailwind = SwiftyTailwind()
     
-  try await tailwind.run(
-    input: .init(validating: "Styles/app.css", relativeTo: resourceDirectory),
-    output: .init(validating: "styles/app.generated.css", relativeTo: publicDirectory),
-    options: .content("Resources/Templates/**/*.mustache")
-  )
+    try await tailwind.run(
+        input: .init(validating: "Styles/app.css", relativeTo: resourceDirectory),
+        output: .init(validating: "styles/app.generated.css", relativeTo: publicDirectory),
+        options: .content("Resources/Templates/**/*.mustache")
+    )
+}
+
+extension ApplicationProtocol {
+    var directory: DefineWorkingDirectory {
+        DefineWorkingDirectory.detect()
+    }
+}
+
+
+struct DefineWorkingDirectory {
+    var workingDirectory: String
+    var resourcesDirectory: String
+    var publicDirectory: String
+    
+    init(workingDirectory: String) {
+        self.workingDirectory = workingDirectory + "/"
+        self.resourcesDirectory = self.workingDirectory + "Resources/"
+        self.publicDirectory = self.workingDirectory + "Public/"
+    }
+    
+    static func detect() -> DefineWorkingDirectory {
+        if let cwd = getcwd(nil, Int(PATH_MAX)) {
+            defer { free(cwd) }
+            return DefineWorkingDirectory(workingDirectory: String(cString: cwd))
+        }
+        return DefineWorkingDirectory(workingDirectory: "./")
+    }
 }
